eb695f9e42f79e281f1d5dc5a98bb71d32c67a39
Elif Öksüzali <76436843+elifoksuzali@users.noreply.github.com>
2021-05-02 01:50:40 +0300

    Add files via upload

---
+++ etu.js
@@ -0,0 +1,12 @@
+
+function acSolMenu(){
+	document.getElementById("solMenuNav").style.width="250px";
+	document.getElementById("main").style.marginLeft="250px";
+	//document.body.style.backgroundColor="rgba(0,0,0,0.4)";
+}
+
+function kapatSolMenu(){
+	document.getElementById("solMenuNav").style.width="0px";
+	document.getElementById("main").style.marginLeft="0px";
+	//document.body.style.backgroundColor="#FFF";
+}
